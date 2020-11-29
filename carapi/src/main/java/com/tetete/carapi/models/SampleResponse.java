bac2cc5220b6cf3fb4bc54ff57a96ec2a0fe833e
神浦晃行 <akiyuki@Akiyuki-no-MBP.local>
2020-11-29 12:29:41 +0900

    initial commit

---
+++ carapi/src/main/java/com/tetete/carapi/models/SampleResponse.java
@@ -0,0 +1,15 @@
+package com.tetete.carapi.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class SampleResponse {
+    private String code;
+    private String value;
+}
