2e3547be7e09440ad9a9f9af6972ad4af6f0e667
神浦晃行 <akiyuki@Akiyuki-no-MBP.local>
2020-12-14 01:53:16 +0900

    detail page details

--- carweb/src/components/organisms/RecommendedMoviesSideBar.vue
+++ carweb/src/components/organisms/RecommendedMoviesSideBar.vue
@@ -1,50 +1,49 @@
 <template>
   <v-list width="100%">
+    <v-subheader>関連動画</v-subheader>
+    <v-list-item-group>
+      <v-list-item v-for="mov in movies" :key="mov">
+        <v-container fluid>
+          <v-row>
+            <v-col cols="6" class="pa-0">
+              <v-img src="https://picsum.photos/id/11/500/300" />
+            </v-col>
+            <v-col>
+              <h5>関連動画テストタイトル</h5>
+            </v-col>
+          </v-row>
+        </v-container>
+      </v-list-item>
+    </v-list-item-group>
 
-		<v-subheader>関連動画</v-subheader>
-		<v-list-item-group>
-			<v-list-item v-for="mov in movies" :key="mov">
-				<v-container fluid>
-					<v-row>
-						<v-col cols="6" class="pa-0">
-							<v-img src="https://picsum.photos/id/11/500/300" />
-						</v-col>
-						<v-col>
-							<h5>関連動画テストタイトル</h5>
-						</v-col>
-					</v-row>
-				</v-container>
-			</v-list-item>
-		</v-list-item-group>
-
-		<v-subheader>おすすめ動画</v-subheader>
-		<v-list-item-group>
-			<v-list-item v-for="mov in movies" :key="mov">
-				<v-container fluid>
-					<v-row>
-						<v-col cols="6" class="pa-0">
-							<v-img src="https://picsum.photos/id/11/500/300" />
-						</v-col>
-						<v-col>
-							<h5>おすすめ動画テストタイトル</h5>
-						</v-col>
-					</v-row>
-				</v-container>
-			</v-list-item>
-		</v-list-item-group>
-	</v-list>
+    <v-subheader>おすすめ動画</v-subheader>
+    <v-list-item-group>
+      <v-list-item v-for="mov in movies" :key="mov">
+        <v-container fluid>
+          <v-row>
+            <v-col cols="6" class="pa-0">
+              <v-img src="https://picsum.photos/id/11/500/300" />
+            </v-col>
+            <v-col>
+              <h5>おすすめ動画テストタイトル</h5>
+            </v-col>
+          </v-row>
+        </v-container>
+      </v-list-item>
+    </v-list-item-group>
+  </v-list>
 </template>
 
 <script>
 export default {
   data: function() {
-		return {
-			movies: Array(6).fill().map((_, v) => v)
-		}
-	}
-}
+    return {
+      movies: Array(6)
+        .fill()
+        .map((_, v) => v)
+    };
+  }
+};
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
--- carweb/src/components/pages/DetailPage.vue
+++ carweb/src/components/pages/DetailPage.vue
@@ -1,56 +1,73 @@
 <template>
   <v-container fluid>
-		<v-row class="ps-3">
-			<v-col cols="9" class="pa-0">
-				<v-card class="rounded-0" outlined>
-					<div class="iframe-wrapper">
-						<iframe src="https://www.youtube.com/embed/AaYTtj2LFFw" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-					</div>
-				</v-card>
-			</v-col>
-			<v-col cols="3" class="pa-0">
-				<recommended-movies-side-bar />
-			</v-col>
-		</v-row>
+    <v-row class="ps-3">
+      <v-col cols="9" class="pa-0">
+        <v-card class="rounded-0" outlined>
+          <div class="iframe-wrapper">
+						<iframe width="560" height="315" src="https://www.youtube.com/embed/AaYTtj2LFFw" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+          </div>
+          <v-card-title>Creepy Nuts / 板の上の魔物【MV】</v-card-title>
+          
+					
+					<v-card-text>
+						<v-btn color="pink lighten-1" dark>コレダレ</v-btn>
+						<v-btn color="pink lighten-1" dark>お気に入りに保存</v-btn>
+						<v-chip-group
+							v-model="selection"
+							active-class="deep-purple accent-4 white--text"
+							column
+						>
+							<v-chip>5:30PM</v-chip>
+							<v-chip>7:30PM</v-chip>
+							<v-chip>8:00PM</v-chip>
+							<v-chip>9:00PM</v-chip>
+						</v-chip-group>
+					</v-card-text>
+					<v-divider></v-divider>
+					aaa
+        </v-card>
+      </v-col>
+      <v-col cols="3" class="pa-0">
+        <recommended-movies-side-bar />
+      </v-col>
+    </v-row>
   </v-container>
 </template>
 
 <script>
-import RecommendedMoviesSideBar from '../organisms/RecommendedMoviesSideBar.vue'
+import RecommendedMoviesSideBar from "../organisms/RecommendedMoviesSideBar.vue";
 export default {
   components: {
     RecommendedMoviesSideBar
-	},
-	data: function () {
-		return {
-			movieId: this.$route.params.id
-		}
-	},
-	created: function () {
-		this.fetchData()
-	},
-	watch: {
-    '$route': 'fetchData'
   },
-	methods: {
-		fetchData: function() {
-
-		}
-	}
-}
+  data: function() {
+    return {
+      movieId: this.$route.params.id
+    };
+  },
+  created: function() {
+    this.fetchData();
+  },
+  watch: {
+    $route: "fetchData"
+  },
+  methods: {
+    fetchData: function() {}
+  }
+};
 </script>
 
 <style lang="scss" scoped>
 .iframe-wrapper {
-	position: relative;
-	width: 100%;
-	padding: calc(315 / 560 * 100%) 0 0;
+  position: relative;
+  width: 100%;
+  padding: calc(315 / 560 * 100%) 0 0;
 }
 iframe {
-	position: absolute;
-	top: 0;
-	left: 0;
-	width: 100%;
-	height: 100%;
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
 }
 </style>
